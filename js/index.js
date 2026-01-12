2c0510275dde58b6ecef4fa62ca31cd80cc28f04
Isntnot <tfsena18@gmail.com>
2026-01-12 11:23:51 -0300

    - Change the index.js location - Remade the main page

--- js/index.js
+++ js/index.js
@@ -5,12 +5,32 @@ const channels = [
   {slider: "blue-slide", counter:"blue-number"},
 ]
 var output = document.getElementById("color-block");
-
+function loaddefault() {
+  channels.forEach(({ slider, counter }) => {
+  let value = localStorage.getItem(slider)
+  if (value == null){
+    value = 0;
+  }
+  const s = document.getElementById(slider);
+  const c = document.getElementById(counter);
+  // Slider → Counter
+  s.value = value
+  c.value = value
+})
+ colorchange()
+}
 function colorchange() {
   const value = channels.map( c => document.getElementById(c.slider).value );
-  
+  channels.forEach(c => {
+  localStorage.setItem(
+    c.slider,
+    document.getElementById(c.slider).value
+  );
+});
+
   output.style.backgroundColor = `rgb(${value.join(',')})`;
 }
+loaddefault()
 channels.forEach(({ slider, counter }) => {
   const s = document.getElementById(slider);
   const c = document.getElementById(counter);
